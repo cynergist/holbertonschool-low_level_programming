6ee24ebc6c7a056ad5622cb5d1df135173aa1049
Cyn <cmdominguez@gmail.com>
2019-02-07 06:34:49 +0000

    add 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a string using printf
+ * Return: 0
+ */
+
+int main(void)
+{
+	puts("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}

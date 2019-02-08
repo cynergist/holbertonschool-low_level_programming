e82113c50223099b885f0874c784c3ba1f17c647
Cyn <cmdominguez@gmail.com>
2019-02-08 00:28:25 +0000

    Removing repeated words in if statements

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,20 +15,19 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
+	printf("Last digit of ");
+
 	if (n % 10 > 5)
 {
-/* if condition is true then print the following */
-	printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+	printf("%d is %d and is greater than 5\n", n, n % 10);
 }
 	else if ((n % 10 < 6) && (n % 10 != 0))
 {
-/* if condition is false then print the following */
-	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
+	printf("%d is %d and is less than 6 and not 0\n", n, n % 10);
 }
 	else
 {
-/* executes when none of the above conditions are true */
-	printf("Last digit of %d is %d and is zero\n", n, n % 10);
+	printf("%d is %d and is zero\n", n, n % 10);
 }
 	return (0);
 }

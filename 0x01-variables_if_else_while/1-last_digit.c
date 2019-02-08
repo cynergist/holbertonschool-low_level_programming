3ecd6659253f7e4cb1e6305e011a56a8c499fc86
Cyn <cmdominguez@gmail.com>
2019-02-08 00:53:33 +0000

    Changing zero to 0

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,7 +16,6 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	printf("Last digit of ");
-
 	if (n % 10 > 5)
 	{
 		printf("%d is %d and is greater than 5\n", n, n % 10);
@@ -27,7 +26,7 @@ int main(void)
 	}
 	else if (n % 10 == 0)
 	{
-		printf("%d is %d and is zero\n", n, n % 10);
+		printf("%d is %d and is 0\n", n, n % 10);
 	}
 	return (0);
 }

4a359d2f8bfb582ad04a1f54f6faf20dfd17cc44
Cyn <cmdominguez@gmail.com>
2019-02-14 07:37:05 +0000

    Fix #ifndef directive

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,7 +1,6 @@
-#ifndef
-HOLBERTON_HEADER
-#define
-HOLBERTON_HEADER
+#ifndef HOLBERTON
+#define HOLBERTON
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -14,4 +13,5 @@ void jack_bauer(void);
 void times_table(void);
 int add(int a, int b);
 void print_to_98(int n);
+
 #endif

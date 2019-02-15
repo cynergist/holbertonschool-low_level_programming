2105cbc4e059d24e3af44c3a3467b88e4a93398e
Cyn <cmdominguez@gmail.com>
2019-02-15 00:33:11 +0000

    add 4-print_most_numbers.c

---
+++ 0x03-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * print_most_numbers - prints numbers from 0 to 9 except 2 and 4,
+ * followed by a new line.
+ *
+ * Description - Uses _putchar twice only.
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		if (num != '2' && num != '4')
+		{
+			_putchar(num);
+		}
+	}
+	_putchar ('\n');
+}

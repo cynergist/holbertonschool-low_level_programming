58e531ac69285f2d8a340ec2983fbdbd3093a45f
Cyn <cmdominguez@gmail.com>
2019-02-21 06:13:36 +0000

    add 7-puts_half.c

---
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+int _strlen(char *s);
+
+/**
+ * puts_half - a function that prints the second half of a string.
+ * @str: pointer to a char.
+ *
+ * Return: nothing.
+ */
+
+void puts_half(char *str)
+{
+	int len, i;
+
+	len = _strlen(str);
+
+	for (i = len / 2; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: a variable declared as a character.
+ *
+ * Description: uses a function similar to strlen.
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	return (length);
+}

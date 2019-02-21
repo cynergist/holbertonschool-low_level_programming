8b4fe3542ec486c7799698dcc73e667bbe5e24a3
Cyn <cmdominguez@gmail.com>
2019-02-21 01:27:41 +0000

    add 0-reset_to_98.c

---
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - Takes a pointer to an int as parameter to update value to 98.
+ * @n: a variable declared as n integer.
+ * If size is less than 0, function to print a \n
+ *
+ * Description: Updates value of *n.
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

6b34ec8ac1ae38114803437ef4a07215ec599a8f
Cyn <cmdominguez@gmail.com>
2019-02-25 22:43:33 +0000

    editing comments for brevity

--- 0x06-pointers_arrays_strings/0-memset.c
+++ 0x06-pointers_arrays_strings/0-memset.c
@@ -2,8 +2,8 @@
 
 /**
  * _memset - a function that fills memory with a constant byte.
- * @n: first bytes of memory area, or the value to be set.
- * @s: points to a block of memory to fill: n
+ * @n: unsigned int and first bytes of memory area, or the value to be set.
+ * @s: points to a block of memory to fill.
  * @b: constant bytes to be set to the value.
  *
  * Return: pointer to memory area s.
@@ -11,7 +11,6 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
-
 	/* this should be an unsigned int */
 	for (i = 0; i < n; i++)
 		s[i] = b;

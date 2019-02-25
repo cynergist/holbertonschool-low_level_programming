4c5a4533ab6f241bf0af8b8114b02a984e47cbde
Cyn <cmdominguez@gmail.com>
2019-02-25 23:16:27 +0000

    indentation edit

--- 0x06-pointers_arrays_strings/0-memset.c
+++ 0x06-pointers_arrays_strings/0-memset.c
@@ -1,18 +1,18 @@
 #include "holberton.h"
-
 /**
  * _memset - a function that fills memory with a constant byte.
- * @n: unsigned int and first bytes of memory area, or the value to be set.
  * @s: points to a block of memory to fill.
  * @b: constant bytes to be set to the value.
+ * @n: bytes of memory area pointed to by s.
  *
  * Return: pointer to memory area s.
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
-	/* this should be an unsigned int */
+
 	for (i = 0; i < n; i++)
 		s[i] = b;
+
 	return (s);
 }

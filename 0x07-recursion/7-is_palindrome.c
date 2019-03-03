8ca4b2f029ac0e4a78d62e8c88633d7373d77238
Cyn <cmdominguez@gmail.com>
2019-03-03 02:10:33 +0000

    replace strlen with recursive function

--- 0x07-recursion/7-is_palindrome.c
+++ 0x07-recursion/7-is_palindrome.c
@@ -1,5 +1,25 @@
 #include "holberton.h"
-#include <string.h>
+
+/**
+ * _strlen_recursion - returns the length of a string.
+ * @s: a string.
+ * Description - Uses a function similar to strlen.
+ *
+ * Return: length of a string.
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+/*No chars in a string would be zero, so return zero. */
+		return (0);
+	}
+	else
+	{
+/* Add one before calling _strlen_recursion on the address of the next char */
+		return (1 + _strlen_recursion(s + 1));
+	}
+}
 
 /**
  * help_palindrome - checks if first and last chars match.
@@ -36,11 +56,12 @@ int help_palindrome(char *s, int first, int last)
 
 int is_palindrome(char *s)
 {
-	int n = strlen(s);
-
-	if (n == 0)
+	int len;
+/* Let variable named len be the _strlen_recursion on string */
+	len = _strlen_recursion(s);
+	if (len == 0)
 /* If the string is empty, return true */
 		return (1);
-/* Otherwise call help_palindrome on string, index 0, & strlen - 1 */
-		return (help_palindrome(s, 0, n - 1));
+/* Otherwise call help_palindrome on string, index 0, & len - 1 */
+		return (help_palindrome(s, 0, len - 1));
 }

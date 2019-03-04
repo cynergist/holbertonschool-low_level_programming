434abb197588ad1217b69c13d9e50c6bd69d530a
Cyn <cmdominguez@gmail.com>
2019-03-04 08:07:17 +0000

    Mismatched file content fix

--- 0x09-argc_argv/2-args.c
+++ 0x09-argc_argv/2-args.c
@@ -2,26 +2,18 @@
 #include <stdio.h>
 
 /**
- * main - prints result of multiplying two numbers.
+ * main - prints all arguments.
  * @argc: argument count.
  * @argv: argument strings.
  * Return: none
  */
 int main(int argc, char *argv[])
 {
-/* Declaring variable result as an int. */
-	int result;
-
-/* If our argument count is zero, one, or two */
-	if (argc <= 2)
+/* As long as argument count is decrementing */
+	while (argc--)
 	{
-/* Display error and a new line, return 1 */
-		printf("Error\n");
-		return (1);
+/* Display each word of the string, followed by a new line */
+		printf("%s\n", *argv++);
 	}
-/* Let result be (string at 1 mult string at 2, using char to int conversion */
-	result = atoi(argv[1]) * atoi(argv[2]);
-/* Display the number of variable result and return 0 */
-	printf("%d\n", result);
 	return (0);
 }

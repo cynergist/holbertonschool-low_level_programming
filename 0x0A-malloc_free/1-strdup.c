e45873df982c82e3810d3059e7141db7eb6daeae
Cyn <cmdominguez@gmail.com>
2019-03-06 08:14:38 +0000

    add 1-strdup.c

---
+++ 0x0A-malloc_free/1-strdup.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strlen - returns a length of a string
+ * @s: a value to be checked
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	return (length);
+}
+/**
+ * _strdup - returns a pointer to a new string whichi is a duplicate of str
+ * @str: a char
+ *
+ * Return: NULL if str is NULL or insufficient mem. Success returns pointer
+ */
+char *_strdup(char *str)
+{
+/* Declared duplicate and index of the array */
+	char *dupe;
+	int i = 0;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+/* Allocating memory to the pointer for the size of char array */
+	dupe = malloc(_strlen(str) * sizeof(char) + 1);
+
+	for (i = 0; i <= _strlen(str); i++)
+	{
+		dupe[i] = str[i];
+	}
+	dupe[i] = '\0';
+	return (dupe);
+	free(dupe);
+}

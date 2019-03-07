52ba24b86c1e1902e2044b7be94bf98a677ebcd5
Cyn <cmdominguez@gmail.com>
2019-03-07 06:41:28 +0000

    add 2-str_concat.c

---
+++ 0x0A-malloc_free/2-str_concat.c
@@ -0,0 +1,50 @@
+#include "holberton.h"
+ #include <stdio.h>
+#include <stdlib.h>
+/**
+ * str_concat - returns a pointer to a space in memory holding s1 and s2
+ * @s1: a char
+ * @s2: a char
+ * Return: NULL if str is NULL or insufficient mem. Success returns pointer
+ */
+char *str_concat(char *s1, char *s2)
+{
+/* Declared duplicate and index of the array */
+	char *p_s1_s2;
+	int size_s1, size_s2, i;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	size_s1 = 0;
+	while (s1[size_s1] != '\0')
+	{
+		size_s1++;
+	}
+	size_s2 = 0;
+	while (s2[size_s2] != '\0')
+	{
+		size_s2++;
+	}
+	p_s1_s2 = malloc((size_s1 + size_s2) * sizeof(char) + 1);
+
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		p_s1_s2[i] = s1[i];
+		i++;
+	}
+	i = 0;
+	while (s2[i] != '\0')
+	{
+/* Start copying s2 after s1 by incrementing size_s1 by i*/
+		p_s1_s2[size_s1 + i] = s2[i];
+		i++;
+	}
+	return (p_s1_s2);
+}

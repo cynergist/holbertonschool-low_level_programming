d2ee6ca8ae39bfa08182ae6cacad05f21340c039
Cyn <cmdominguez@gmail.com>
2019-03-08 22:09:42 +0000

    removing white space

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -11,10 +11,9 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *p_s1_ns2;
-	int len_s1;
-	unsigned int len_s2;
-	unsigned int i;
-	unsigned int j;
+
+	unsigned int len_s1, len_s2, i, j;
+
 /* Ensuring function proceeds when null is accounted for */
 	if (s1 == NULL)
 		s1 = "";
@@ -23,23 +22,19 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 /* Find length of s1 */
 	len_s1 = 0;
 	while (s1[len_s1] != '\0')
-		{
-			len_s1++;
-		}
+		len_s1++;
 /* Find length of s2 */
 	len_s2 = 0;
 	while (s2[len_s2] != '\0')
-		{
-			len_s2++;
-		}
+		len_s2++;
 /* If n bytes are larger than length of s2, set it to length of s2 */
 	if (n >= len_s2)
 		n = len_s2;
 /* Allocate enough memory for length of s1, plus n, plus 1 */
-	p_s1_ns2 = malloc (len_s1 + n + 1);
+	p_s1_ns2 = malloc(len_s1 + n + 1);
 /* Check for sufficient memory allocation */
 	if (p_s1_ns2 == NULL)
-	        return (NULL);
+		return (NULL);
 /* Copy s1 into memory of p_s1_ns2 */
 	i = 0;
 	while (s1[i] != '\0')

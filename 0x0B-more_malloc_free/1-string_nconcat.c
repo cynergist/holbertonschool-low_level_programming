4704461e9fc57623d666a603216701428a259337
Cyn <cmdominguez@gmail.com>
2019-03-08 22:13:06 +0000

    remove comments under line limit

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -19,30 +19,27 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-/* Find length of s1 */
+/* Find length of s1 and s2 */
 	len_s1 = 0;
 	while (s1[len_s1] != '\0')
 		len_s1++;
-/* Find length of s2 */
 	len_s2 = 0;
 	while (s2[len_s2] != '\0')
 		len_s2++;
 /* If n bytes are larger than length of s2, set it to length of s2 */
 	if (n >= len_s2)
 		n = len_s2;
-/* Allocate enough memory for length of s1, plus n, plus 1 */
+/* Allocate enough memory for length of s1, plus n, plus 1, check for malloc */
 	p_s1_ns2 = malloc(len_s1 + n + 1);
-/* Check for sufficient memory allocation */
 	if (p_s1_ns2 == NULL)
 		return (NULL);
-/* Copy s1 into memory of p_s1_ns2 */
+/* Copy s1 and s2 into memory of p_s1_ns2 */
 	i = 0;
 	while (s1[i] != '\0')
 	{
 		p_s1_ns2[i] = s1[i];
 		i++;
 	}
-/* Copy s2 into memory of p_s1_ns2 up to n */
 	j = 0;
 	while (j < n)
 	{

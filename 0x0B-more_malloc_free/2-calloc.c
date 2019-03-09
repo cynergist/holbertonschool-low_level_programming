2c59625caf5ecb3146f45247282abda6c768b6c0
Cyn <cmdominguez@gmail.com>
2019-03-09 07:44:09 +0000

    add 2-calloc.c

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -1,11 +1,25 @@
 #include "holberton.h"
 
 /**
- * _calloc -
+ * _calloc - allocates memory for an array using malloc
  *
- * Return: 
+ * @nmemb: unsigned int
+ * @size: unsigned int
+ * Return: a pointer to the allocated memory on success. Otherwise, NULL
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-
+	char *charptr;
+	unsigned int i;
+/* Allocate space using charptr */
+	charptr = malloc(nmemb * size);
+/* When to return NULL */
+	if (charptr == NULL || nmemb == 0 || size == 0)
+		return (NULL);
+/* Zero out indices in memory block */
+	for (i = 0; i < nmemb * size; i++)
+	{
+		charptr[i] = 0;
+	}
+	return (charptr);
 }

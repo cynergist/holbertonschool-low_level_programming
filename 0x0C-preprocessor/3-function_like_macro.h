f08e23d3d1dd682246664541927a73b5c3d49a75
Cyn <cmdominguez@gmail.com>
2019-03-11 22:44:59 +0000

    add 3-function_like_macro.h

---
+++ 0x0C-preprocessor/3-function_like_macro.h
@@ -0,0 +1,5 @@
+#ifndef _FUNCTION_LIKE_MACRO_
+#define _FUNCTION_LIKE_MACRO_
+/* Define absolute value of x, if x is less than 0 or neg, make it positive */
+#define ABS(x) ((x < 0) ? (-x) : (x))
+#endif /* _FUNCTION_LIKE_MACRO_ */

7981e67e3f81dd3bfcad6148310459b49bf66f66
Cyn <cmdominguez@gmail.com>
2019-03-12 06:43:42 +0000

    edit to if, else for elements

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -13,11 +13,23 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 /* If there is no name assigned to dog, use "(nil)" */
-	if (!d->name)
-		d->name = "(nil)";
+	if (d->name)
+	{
+		d->name = d->name;
+	}
+		else
+		{
+			d->name = "(nil)";
+		}
 /* If there is no owner assigned to dog, use "(nil)" */
-	if (!d->owner)
+	if (d->owner)
+	{
+		d->owner = d->owner;
+	}
+	else
+		{
 		d->owner = "(nil)";
+		}
 /* Print name, age, and owner */
 	printf("Name: %s \n", d->name);
 	printf("Age: %.6f \n", d->age);

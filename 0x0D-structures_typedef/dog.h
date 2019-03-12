80a810ae354a572da5dbb09fe931806f90a97d10
Cyn <cmdominguez@gmail.com>
2019-03-12 07:11:00 +0000

    adding prototypes and headers

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -1,20 +1,26 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct dog - a structure which defines the characteristics of a dog
- *@name: a char and name of the dog
- *@age: a float and age of the dog
- *@owner: a char and owner of the dog
+ * @name: a char and name of the dog
+ * @age: a float and age of the dog
+ * @owner: a char and owner of the dog
  */
+
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif /* _DOG_H_ */

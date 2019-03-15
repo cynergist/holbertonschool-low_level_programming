e43c3ae9aa18465f61fe78b819eb55dde0bf1aea
Cyn <cmdominguez@gmail.com>
2019-03-15 01:30:27 +0000

    Adding operation functions

--- 0x0E-function_pointers/3-op_functions.c
+++ 0x0E-function_pointers/3-op_functions.c
@@ -1,5 +1,61 @@
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
+#include "3-calc.h"
+
+/**
+ * op_add - a function that adds two integers
+ * @a: an integer
+ * @b: an integer
+ *
+ * Return: 0
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - a function that subtracts two integers
+ * @a: an integer
+ * @b: an integer
+ *
+ * Return: 0
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - a function that multiplies two integers
+ * @a: an integer
+ * @b: an integer
+ *
+ * Return: 0
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - a function that divides two integers
+ * @a: an integer
+ * @b: an integer
+ *
+ * Return: 0
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - a function that mods two integers
+ * @a: an integer
+ * @b: an integer
+ *
+ * Return: 0
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}

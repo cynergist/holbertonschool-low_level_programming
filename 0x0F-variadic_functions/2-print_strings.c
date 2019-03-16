7654cadcb9df8a1473c908d11f2514c516257af9
Cyn <cmdominguez@gmail.com>
2019-03-16 23:03:15 +0000

    add 0x0F-variadic_functions

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -23,6 +23,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		printf("%s", string);
 		if (separator != NULL && i < n - 1)
 			printf("%s", separator);
+		if (separator == NULL)
 		if (string == NULL)
 			printf("(nil)");
 	}

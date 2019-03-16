d2ff98a2ed09fdb22f7be4270378a2f83211d0fe
Cyn <cmdominguez@gmail.com>
2019-03-16 22:22:34 +0000

    Add changes to 2-print_strings

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -12,22 +12,21 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list ap;
 	unsigned int i;
-	char *string;
 /* Initializing arguments to store all values after n */
 	va_start(ap, n);
 
 /* This loops through all the other arguments. n contains count of variables */
 	for (i = 0; i < n; i++)
 	{
-		string = va_arg(ap, char *);
-		if (!string)
-			printf("(nil)");
-		if (!separator || separator == NULL)
+		char *string = va_arg(ap, char *);
+
+		printf("%s", string);
+		if (separator != NULL && i < n - 1)
 			printf("%s", separator);
-		else
-			printf("%s", string);
+		if (string == NULL)
+			printf("(nil)");
 	}
 /* Cleans up the list */
-	va_end(ap);
 	printf("\n");
+	va_end(ap);
 }

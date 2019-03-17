e2ec28a6437def1947a42c148e0275cb4c797a36
Cyn <cmdominguez@gmail.com>
2019-03-17 04:10:29 +0000

    fix assignment of sum

--- 0x0F-variadic_functions/0-sum_them_all.c
+++ 0x0F-variadic_functions/0-sum_them_all.c
@@ -5,6 +5,7 @@
  * sum_them_all - returns a sum of all parameters
  *
  * @n: an unsigned integer
+ * @...: any other arguments
  * Return: sum of parameters or zero if n is 0
  */
 int sum_them_all(const unsigned int n, ...)
@@ -21,11 +22,8 @@ int sum_them_all(const unsigned int n, ...)
 		return (0);
 /* Sum all the parameters. We rely on the function calls to tell us how many */
 	for (i = 0; i < n; i++)
-	{
-		sum += va_arg(ap, int);
-	}
+		sum = (sum + va_arg(ap, int));
 /* Cleans up the list */
 	va_end(ap);
-
 	return (sum);
 }

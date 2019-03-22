1e7851aa50def565513942217438d0eaa3064fbf
Cyn <cmdominguez@gmail.com>
2019-03-22 05:49:25 +0000

    edit %d to %d

--- 0x11-singly_linked_lists/0-print_list.c
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -13,7 +13,7 @@ size_t print_list(const list_t *h)
 {
 /* Initialize node_num variable of type size_t at zero */
 	size_t node_num = 0;
-/* As long as pointer to element at list_t is not NULL */
+/* As long as pointer to list_t is not NULL */
 	while (h)
 	{
 /* If pointer to string at list_t is NULL, print [0] (nil) */
@@ -21,8 +21,8 @@ size_t print_list(const list_t *h)
 			printf("[0] (nil)\n");
 /* Otherwise print number of chars is string and string */
 		else
-			printf("[%d] %s\n", h->len, h->str);
-/* Reassign h pointer to the next node in the list */
+			printf("[%i] %s\n", h->len, h->str);
+/* Reassign pointer to the next node in the list */
 		h = h->next;
 /* Increment node_num iterator until reaching NULL and return it */
 		node_num++;

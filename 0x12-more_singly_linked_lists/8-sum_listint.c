16fc8c63f0f08e14faf5cab725b8276ac228666e
Cyn <cmdominguez@gmail.com>
2019-03-27 06:37:29 +0000

    add 8-sum_listint.c

---
+++ 0x12-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * sum_listint - a function that returns the sum of all data in a listint_t list
+ * @head: a pointer to head of the list
+ *
+ * Return: sum of n
+ */
+int sum_listint(listint_t *head)
+{
+/* if list is empty, return 0 */
+}

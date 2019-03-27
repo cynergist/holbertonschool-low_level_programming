2009b7e59fca631cf87e34147f0c8ad94fdbb90a
Cyn <cmdominguez@gmail.com>
2019-03-27 06:43:46 +0000

    add 9-insert_nodeint.c

---
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * insert_nodeint_at_index - a function that inserts a new node at nth position
+ * @head: a pointer to head of the list
+ * @idx: index of the list where new node should be added
+ * @n: integer
+ * Return: address of the new node
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+
+/* Returns the address of the new node, or NULL if it failed */
+/* if not possible to add the new node at index idx, return NULL */
+}

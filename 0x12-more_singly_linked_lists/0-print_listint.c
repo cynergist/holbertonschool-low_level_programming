7e7cdbd60b87e6487edc2c6d8ed6a2a7c9f6226f
Cyn <cmdominguez@gmail.com>
2019-03-27 05:36:27 +0000

    remove white space

--- 0x12-more_singly_linked_lists/0-print_listint.c
+++ 0x12-more_singly_linked_lists/0-print_listint.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 /**
  * print_listint - a function that prints all elements of a listint_t list
- * @h: a pointer to a listint_t
+ * @h: a pointer to head of listint_t
  *
- * Return: 0
+ * Return: number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
@@ -13,7 +13,7 @@ size_t print_listint(const listint_t *h)
 /* As long as pointer to listint_t is not NULL */
 	while (h)
 	{
-		printf("%i \n", h->n);
+		printf("%i\n", h->n);
 /* Reassign pointer to the next node in the list */
 		h = h->next;
 /* Increment iterator until reaching NULL terminator in the last node */

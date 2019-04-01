354b15801ae0c3b53da2ccb19372089ee00a6d95
Cyn <cmdominguez@gmail.com>
2019-04-01 05:07:48 +0000

    add 9-insert_nodeint.c

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,7 +9,37 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+	listint_t *new_node, *node;
+	unsigned int i = 0;
+/* Create space for new_node */
+	new_node = malloc(sizeof(listint_t));
+/* Check if idx exists so we can cadd new_node at idx */
+	if (!idx)
+/* If idx is NULL, new_node points to n value */
+	{
+		new_node->n = n;
+/* Set new_node->next to head and update head pointer */
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+/* Implicit "else" here. Now setting head pointer to "node" to indicate shift*/
+	node = *head;
 
+	while (node)
+	{
+/* As long as node is not NULL if our counter i is at position before idx */
+		if (i == idx - 1)
+		{
+			new_node->next = node->next;
+			node->next = new_node;
+			return (new_node);
+		}
+		i++;
+		node = node->next;
+	}
 /* Returns the address of the new node, or NULL if it failed */
-/* if not possible to add the new node at index idx, return NULL */
+	free(new_node);
+	return (NULL);
 }
+/* if not possible to add the new node at index idx, return NULL */

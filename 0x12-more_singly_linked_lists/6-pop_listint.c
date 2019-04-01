62e197168746519b5fa0212049926dfe48e644e4
Cyn <cmdominguez@gmail.com>
2019-04-01 05:42:50 +0000

    add 6-pop_listint.c

--- 0x12-more_singly_linked_lists/6-pop_listint.c
+++ 0x12-more_singly_linked_lists/6-pop_listint.c
@@ -4,24 +4,20 @@
  * pop_listint - a function that deletes the head node of a listint_t list
  * @head: a pointer to head of the list
  *
- * Return: head node data
+ * Return: head node's data(n)
  */
 int pop_listint(listint_t **head)
 {
 /* Declare new node and head pointers */
-	listint_t *new_node, *head_new_node;
+	listint_t *node;
+	int n;
 
-	if (head == NULL)
+	if (head == NULL || *head == NULL)
 		return (0);
 
-	head_new_node = *head;
-/* As long as head pointer is not NULL */
-	while (head_new_node)
-	{
-		new_node = head_new_node->next;
-		free(head_new_node);
-		head_new_node = new_node;
-	}
-	*head = NULL;
+	node = (*head)->next;
+	n = (*head)->n;
+	free(*head);
+	*head = node;
 	return (n);
 }

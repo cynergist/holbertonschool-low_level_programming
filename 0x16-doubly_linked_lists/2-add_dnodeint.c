2d55a3eb563123e1500e23da25aafdf950b02011
Cyn <cmdominguez@gmail.com>
2019-05-05 22:49:49 +0000

    incorporated pointer to previous correctly

--- 0x16-doubly_linked_lists/2-add_dnodeint.c
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -8,18 +8,20 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new_node;
-
-	new_node = malloc(sizeof(dlistint_t));
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 
 	if (!new_node)
+	{
 		return (NULL);
+	}
 		new_node->n = n;
 		new_node->next = *head;
-		*head = new_node;
+		new_node->prev = NULL;
 
-		if (*head == NULL)
-			return (NULL);
-/* implicit else */
-			return (*head);
+		if (*head != NULL)
+		{
+			(*head)->prev = new_node;
+		}
+		*head = new_node;
+		return (*head);
 }

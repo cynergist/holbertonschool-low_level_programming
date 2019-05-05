00fe1dd088ea8e6b1614ac59b5c32e4763400f41
Cyn <cmdominguez@gmail.com>
2019-05-05 21:53:22 +0000

    refactored to check for head equals null case

--- 0x16-doubly_linked_lists/2-add_dnodeint.c
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -17,7 +17,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		new_node->n = n;
 		new_node->next = *head;
 		*head = new_node;
-		return (*head);
-		if (!*head)
+
+		if (*head == NULL)
 			return (NULL);
+/* implicit else */
+			return (*head);
 }

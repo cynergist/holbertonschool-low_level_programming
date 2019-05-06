cd0887c501d5d3fb4714ada572f8aa3416e10521
Cyn <cmdominguez@gmail.com>
2019-05-06 00:35:56 +0000

    remove frees

--- 0x16-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x16-doubly_linked_lists/3-add_dnodeint_end.c
@@ -19,11 +19,11 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 /* New node is at the head automatically */
 		new_node->n = n;
 		new_node->next = NULL;
-		new_node->prev = *head;
 
 		if (*head == NULL)
 		{
 			*head = new_node;
+			new_node->prev = *head;
 			return (new_node);
 		}
 /* As long as the end node is the following address and not null */

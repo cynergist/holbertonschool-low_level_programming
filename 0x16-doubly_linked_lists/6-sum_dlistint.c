240bc457f6bcf48b377089e75b690ac8684385e4
Cyn <cmdominguez@gmail.com>
2019-05-06 04:26:41 +0000

    add 6-sum_dlistint.c

---
+++ 0x16-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * sum_dlistint - returns the sum of all data of a linked list
+ * @head: a pointer to a head of struct of type dlistint_t
+ *
+ * Return: sum of n
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+	dlistint_t *current = head;
+
+	current->prev = head;
+	while (current)
+	{
+		sum = sum + current->n;
+		current = current->next;
+	}
+	return (sum);
+}

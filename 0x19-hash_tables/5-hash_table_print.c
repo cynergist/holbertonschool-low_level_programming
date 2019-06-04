47ff014da0d5b30c7aa9b1f28e6f4dd05526cf95
Cyn <cmdominguez@gmail.com>
2019-06-04 00:50:12 +0000

    fix for space after colon

--- 0x19-hash_tables/5-hash_table_print.c
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -9,23 +9,24 @@ void hash_table_print(const hash_table_t *ht)
 	unsigned long int i = 0, flag = 0;
 	hash_node_t *current;
 
-	if (ht)
+	if (!ht)
+		return;
 	{
-		printf("{");
-		for (; i < ht->size; i++)
+	printf("{");
+	for (; i < ht->size; i++)
+	{
+		current = ht->array[i];
+		while (current)
 		{
-			current = ht->array[i];
-			while (current)
-			{
-				if (current->key)
-					flag++;
-					if (flag == 1)
-						printf("\'%s\' : \'%s\'", current->key, current->value);
-					else
-						printf(", \'%s\' : \'%s\'", current->key, current->value);
-				current = current->next;
-			}
+			if (current->key)
+				flag++;
+			if (flag == 1)
+				printf("\'%s\': \'%s\'", current->key, current->value);
+			else
+				printf(", \'%s\': \'%s\'", current->key, current->value);
+		current = current->next;
 		}
+	}
 		printf("}\n");
 	}
 }

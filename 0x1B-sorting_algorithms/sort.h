8a0bc45211afffb3117cc445843e18eae3da505a
Cyn <cmdominguez@gmail.com>
2019-06-15 20:58:59 +0000

    add sort.h

---
+++ 0x1B-sorting_algorithms/sort.h
@@ -0,0 +1,99 @@
+#ifndef SORT_H
+#define SORT_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * print_array - Prints an array of integers
+ *
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
+void print_array(const int *array, size_t size)
+{
+	size_t i;
+
+	i = 0;
+	while (array && i < size)
+	{
+		if (i > 0)
+			printf(", ");
+		printf("%d", array[i]);
+		++i;
+	}
+	printf("\n");
+}
+/**
+ * print_list - Prints a list of integers
+ *
+ * @list: The list to be printed
+ */
+void print_list(const listint_t *list)
+{
+	int i;
+
+	i = 0;
+	while (list)
+	{
+		if (i > 0)
+			printf(", ");
+		printf("%d", list->n);
+		++i;
+		list = list->next;
+	}
+	printf("\n");
+}
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+/* 0-bubble_sort.c */
+void bubble_sort(int *array, size_t size);
+
+/* 1-insertion_sort_list.c */
+void insertion_sort_list(listint_t **list);
+
+/* 2-selection_sort.c */
+void selection_sort(int *array, size_t size);
+
+/* 3-quick_sort.c */
+void quick_sort(int *array, size_t size);
+
+/* 100-shell_sort.c */
+void shell_sort(int *array, size_t size);
+
+/* 101-cocktail_sort_list.c */
+void cocktail_sort_list(listint_t **list);
+
+/* 102-counting_sort.c */
+void counting_sort(int *array, size_t size);
+
+/* 103-merge_sort.c */
+void merge_sort(int *array, size_t size);
+
+/* 104-heap_sort.c */
+void heap_sort(int *array, size_t size);
+
+/* 105-radix_sort.c */
+void radix_sort(int *array, size_t size);
+
+/* 106-bitonic_sort.c */
+void bitonic_sort(int *array, size_t size);
+
+/* 107-quick_sort_hoare.c */
+void quick_sort_hoare(int *array, size_t size);
+
+/* 1000-sort_deck.c */
+void sort_deck(deck_node_t **deck);
+
+#endif /* SORT_H */

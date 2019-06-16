ea87effc70be9c11844f90da8cbe324c6f461739
Cyn <cmdominguez@gmail.com>
2019-06-16 02:44:24 +0000

    fixing sources

--- 0x1B-sorting_algorithms/bubble.mk
+++ 0x1B-sorting_algorithms/bubble.mk
@@ -1,13 +1,18 @@
-CC = gcc
+# The compiler school requires
+GCC = gcc
+# Compiler flags required by tasks at compilation time
 CFLAGS = -Wall -Werror -Wextra -pedantic
+# Header file used
 DEPS = sort.h
-SRCS = $(0-bubble_sort.c) $(0-main.c) $(print_array.c)
+
+SRCS = $(0-bubble_sort) $(0-main) $(print_array)
+
 OBJS = (SRCS:.c=.o)
 PROG = cmd
 
 cmd: 	$(DEPS)
-	@echo "testing $(CC)"
-	$(CC) $(CFLAGS) $(OBJS) -o $@
+
+	$(GCC) $(CFLAGS) $(SRCS) -o $@
 
 all: $(PROG)
 

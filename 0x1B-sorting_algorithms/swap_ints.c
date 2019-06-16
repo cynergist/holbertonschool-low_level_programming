e9da93ff8253166c5b0c8ee3c864ab7ae1f38508
Cyn <cmdominguez@gmail.com>
2019-06-16 02:47:05 +0000

    add swap_ints.c

---
+++ 0x1B-sorting_algorithms/swap_ints.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+/**
+ * swap_ints - a function that swaps two integers
+ *
+ * @a: first integer
+ * @b: second integer
+ */
+void swap_ints(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}

82d8523f0d1ecb4fe3950296703d7d8c96a63d4e
Cyn <cmdominguez@gmail.com>
2019-06-16 02:24:18 +0000

    add 0-bubble_sort.c

---
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+/**
+ * bubble_sort - a function that sorts
+ * numbers in chronological order
+ * @array: an series of integers
+ * @size: the size of our array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	unsigned int i = 0;
+	int sorting = 1;
+
+	while (sorting)
+		/* handles the length of the array */
+	{
+		sorting = 0;
+		while (i < (size - 1))
+			/* handles a consecutive pair of ints in the array */
+		{
+			if (array[i] > array[i + 1])
+			{
+				swap_ints(&array[i], &array[i + 1]);
+				print_array(array, size);
+				sorting = 1;
+			}
+		i++;
+		}
+		i = 0;
+	}
+}

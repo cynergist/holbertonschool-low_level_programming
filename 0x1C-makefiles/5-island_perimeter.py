e043100f8fdf27f95f1586a118112f8f50d744db
Cyn <cmdominguez@gmail.com>
2019-07-12 06:54:46 +0000

    adding if __name__ and fix pep8

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    ''' Module for island perimeter '''
+
+    def island_perimeter(grid):
+        ''' Returns the perimeter of the island described in grid '''
+        grid = []

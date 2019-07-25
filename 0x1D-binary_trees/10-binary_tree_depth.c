f9bf3593b6a747a22154fc29646ec4aca6532c04
Cyn <cmdominguez@gmail.com>
2019-07-25 09:19:58 +0000

    Add documentation of recursive method to find depth number

--- 0x1D-binary_trees/10-binary_tree_depth.c
+++ 0x1D-binary_trees/10-binary_tree_depth.c
@@ -7,10 +7,14 @@
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	int i = 0;
-
+	/* If pointer is NULL return 0 */
 	if (!tree)
 		return (0);
+	/* We remember that tree->left and tree->right sends us down-tree */
+	/* While tree->parent send us up-tree */
 	if (tree->parent)
 		i = binary_tree_depth(tree->parent) + 1;
+	/* Each recursive pass adds 1 til we are at root, which has no parent */
+	/* The number of recursive passes is also our depth number */
 	return (i);
 }

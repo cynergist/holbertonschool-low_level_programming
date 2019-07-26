3397e0300b5e47e34390fe704e34beb375d45eb2
Cyn <cmdominguez@gmail.com>
2019-07-26 16:54:46 +0000

    Solve recursion call in main function

--- 0x1D-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1D-binary_trees/16-binary_tree_is_perfect.c
@@ -13,12 +13,13 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	if (binary_tree_is_perfect(tree->left) ==
-	    binary_tree_is_perfect(tree->right))
+	if (binary_tree_height(tree->left) ==
+	    binary_tree_height(tree->right))
 	{
 		left = tree->left;
 		right = tree->right;
 		/* if nodes are not a leaf and left and right are null */
+		if (!left && !right)
 		{
 			/* it will be a perfect tree */
 			return (1);
@@ -39,21 +40,17 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	int left_height, right_height;
+	int left_height = 0, right_height = 0;
 
 	if (!tree)
 		return (0);
-	/* If pointer is on a leaf */
-	else if (!tree->left && !tree->right)
-		return (0);
-	{
+
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 
 	if (left_height > right_height)
-		/* Take the greater height and add 1 for parent */
+		/* Take the greater height and add 1 for root */
 		return (left_height + 1);
 	else
 		return (right_height + 1);
-	}
 }

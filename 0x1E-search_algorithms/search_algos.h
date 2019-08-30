77ab2c965bca0642abd88b158ca5effd7e3f4d98
Cyn <cmdominguez@gmail.com>
2019-08-30 03:23:56 +0000

    add search_algos.h

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,9 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+#include <stdlib.h>
+#include <stdio.h>
+/* 0-linear.c */
+int linear_search(int *array, size_t size, int value);
+/* 1-binary.c */
+int binary_search(int *array, size_t size, int value);
+#endif /* SEARCH_ALGOS_H */
